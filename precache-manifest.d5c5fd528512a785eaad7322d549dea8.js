4db6c904bfe94ba2a56c2d0ed0dc8bbec69c958d
elysee eve <elysee.eve@codeloccol.org>
2020-10-19 01:31:59 +0100

    Updates

---
+++ precache-manifest.d5c5fd528512a785eaad7322d549dea8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dff238d53a5198eb16e6ccb77b371cd",
+    "url": "/mytube/index.html"
+  },
+  {
+    "revision": "3bbd1611f2127ef31934",
+    "url": "/mytube/static/js/2.04afa3a0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/mytube/static/js/2.04afa3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5174f0581d2dd8b4c8",
+    "url": "/mytube/static/js/main.2b472089.chunk.js"
+  },
+  {
+    "revision": "c6dfff73608a5f0f01d0",
+    "url": "/mytube/static/js/runtime-main.412b532b.js"
+  }
+]);
